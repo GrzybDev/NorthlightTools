b33f3ec199e5cbe129c35e3dfc37ba5712a435ff
Marek Grzyb <marek@grzyb.dev>
2025-07-01 21:10:55 +0200

    Added extract command

--- src/northlighttools/rmdp/__init__.py
+++ src/northlighttools/rmdp/__init__.py
@@ -2,7 +2,16 @@ from pathlib import Path
 from typing import Annotated
 
 import typer
-from rich.progress import Progress
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    SpinnerColumn,
+    TaskProgressColumn,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
 
 from northlighttools.rmdp.helpers import get_archive_paths
 from northlighttools.rmdp.package import Package
@@ -77,6 +86,63 @@ def contents(
     for file in package.files:
         file_path = package.get_file_path(file)
         typer.echo(f"{file_path} (Size: {file.size} bytes, Offset: {file.offset})")
+
+
+@app.command(help="Extracts a Remedy Package")
+def extract(
+    archive_path: Annotated[
+        Path,
+        typer.Argument(
+            help="Path to the input .bin/.rmdp file",
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            readable=True,
+        ),
+    ],
+    output_dir: Annotated[
+        Path | None,
+        typer.Argument(
+            help="Path to the output directory where files will be extracted",
+            file_okay=False,
+            dir_okay=True,
+            writable=True,
+        ),
+    ] = None,
+):
+    bin_path, rmdp_path = get_archive_paths(archive_path)
+
+    output_dir = output_dir or rmdp_path.parent / rmdp_path.stem
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    with Progress(transient=True) as progress:
+        progress.add_task(
+            description="Reading package metadata...",
+            total=None,
+        )
+        package = Package(header_path=bin_path)
+
+    progress = Progress(
+        SpinnerColumn(finished_text="\u2713"),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        TaskProgressColumn(),
+        TimeElapsedColumn(),
+        TimeRemainingColumn(),
+    )
+
+    with progress and rmdp_path.open("rb") as f:
+        for file in progress.track(
+            package.files,
+            description="Extracting files...",
+        ):
+            file_path = package.get_file_path(file)
+            output_path = output_dir / file_path
+
+            progress.console.log(f"Extracting {file_path}...")
+
+            package.extract(f, file, output_path)
 
 
 if __name__ == "__main__":
