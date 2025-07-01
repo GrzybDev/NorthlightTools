225d24c46c70ced5ec121dabcf7e11b73874bce8
Marek Grzyb <marek@grzyb.dev>
2025-07-01 22:19:25 +0200

    Added progress instance as constructor parameter for BinaryFont class

--- src/northlighttools/binfnt/font.py
+++ src/northlighttools/binfnt/font.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
 from northlighttools.binfnt.enumerators.font_version import FontVersion
+from northlighttools.rmdp import Progress
 
 
 class BinaryFont:
 
     __version: FontVersion = FontVersion.QUANTUM_BREAK
 
-    def __init__(self, file_path: Path | None = None):
+    def __init__(self, progress: Progress, file_path: Path | None = None):
+        self.__progress = progress
+
         if file_path is not None:
             self.__load(file_path)
 
