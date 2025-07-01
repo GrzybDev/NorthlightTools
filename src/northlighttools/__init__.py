6085efe5c0561aa2a83ccb889afc12aac6fc2928
Marek Grzyb <marek@grzyb.dev>
2025-07-01 20:16:14 +0200

    Defined typer app for Remedy Packages tools

--- src/northlighttools/__init__.py
+++ src/northlighttools/__init__.py
@@ -1,7 +1,12 @@
 import typer
 
+from northlighttools import rmdp
+
 app = typer.Typer(help="Collection of various tools for Northlight Engine")
 
+app.add_typer(
+    rmdp.app, name="rmdp", help="Tools for Remedy Packages (.bin/.rmdp files)"
+)
 
 if __name__ == "__main__":
     app()
---
+++ src/northlighttools/rmdp/__init__.py
@@ -0,0 +1,7 @@
+import typer
+
+app = typer.Typer(help="Tools for Remedy Packages (.bin/.rmdp files)")
+
+
+if __name__ == "__main__":
+    app()
