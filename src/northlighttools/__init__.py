f9b1a45e63a750f47e0f6a9554f15093a51f50d3
Marek Grzyb <marek@grzyb.dev>
2025-07-01 21:57:35 +0200

    Defined typer app for string table tools

--- src/northlighttools/__init__.py
+++ src/northlighttools/__init__.py
@@ -1,11 +1,17 @@
 import typer
 
-from northlighttools import rmdp
+from northlighttools import rmdp, string_table
 
 app = typer.Typer(help="Collection of various tools for Northlight Engine")
 
 app.add_typer(
     rmdp.app, name="rmdp", help="Tools for Remedy Packages (.bin/.rmdp files)"
+)
+
+app.add_typer(
+    string_table.app,
+    name="string-table",
+    help="Tools for string tables in Remedy games (string_table.bin files)",
 )
 
 if __name__ == "__main__":
---
+++ src/northlighttools/string_table/__init__.py
@@ -0,0 +1,8 @@
+import typer
+
+app = typer.Typer(
+    help="Tools for string tables in Remedy games (string_table.bin files)"
+)
+
+if __name__ == "__main__":
+    app()
