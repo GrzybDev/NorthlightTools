4b158f9d16e2a0cc260180cd2dc6cfe12667ee1d
Marek Grzyb <marek@grzyb.dev>
2025-07-01 20:36:58 +0200

    Added info command

--- src/northlighttools/rmdp/__init__.py
+++ src/northlighttools/rmdp/__init__.py
@@ -1,6 +1,39 @@
+from pathlib import Path
+from typing import Annotated
+
 import typer
+from rich.progress import Progress
+
+from northlighttools.rmdp.helpers import get_archive_paths
+from northlighttools.rmdp.package import Package
 
 app = typer.Typer(help="Tools for Remedy Packages (.bin/.rmdp files)")
+
+
+@app.command(help="Prints information about a Remedy Package")
+def info(
+    archive_path: Annotated[
+        Path,
+        typer.Argument(
+            help="Path to the input .bin/.rmdp file",
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            readable=True,
+        ),
+    ],
+):
+    bin_path, _ = get_archive_paths(archive_path)
+
+    with Progress(transient=True) as progress:
+        progress.add_task(
+            description="Reading package metadata...",
+            total=None,
+        )
+        package = Package(header_path=bin_path)
+
+    typer.echo(f"Endianness: {package.endianness}")
+    typer.echo(f"Version: {package.version.name} ({package.version.value})")
 
 
 if __name__ == "__main__":
