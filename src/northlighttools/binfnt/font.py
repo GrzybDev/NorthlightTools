0d6a8fdecaa47020236308385e39802259b491e8
Marek Grzyb <marek@grzyb.dev>
2025-07-01 22:32:10 +0200

    Finished parser for character block in BinaryFont

--- src/northlighttools/binfnt/font.py
+++ src/northlighttools/binfnt/font.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from struct import unpack
 
+from northlighttools.binfnt.dataclasses.character_rmd import RemedyCharacter
 from northlighttools.binfnt.enumerators.font_version import FontVersion
 from northlighttools.rmdp import Progress
 
@@ -11,9 +13,22 @@ class BinaryFont:
     def __init__(self, progress: Progress, file_path: Path | None = None):
         self.__progress = progress
 
+        self.__characters: list[RemedyCharacter] = []
+
         if file_path is not None:
             self.__load(file_path)
 
     def __load(self, file_path: Path):
         with file_path.open("rb") as reader:
             self.__version = FontVersion(int.from_bytes(reader.read(4), "little"))
+
+            self.__read_character_block(reader)
+
+    def __read_character_block(self, reader):
+        self.__progress.console.log("Reading character block...")
+
+        char_count = int.from_bytes(reader.read(4), "little") // 4
+        self.__characters = []
+
+        for _ in range(char_count):
+            self.__characters.append(RemedyCharacter(*unpack("16f", reader.read(64))))
