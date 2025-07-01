0de078888bda9e733d8ece81d40486f64df82fa2
Marek Grzyb <marek@grzyb.dev>
2025-07-01 23:30:52 +0200

    Added helper function to calculate kerning amount without font size

--- src/northlighttools/binfnt/dataclasses/kerning.py
+++ src/northlighttools/binfnt/dataclasses/kerning.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass
 
+from northlighttools.binfnt.enumerators.font_version import FontVersion
+
 
 @dataclass
 class Kerning:
     first: int
     second: int
     amount: float
+
+    def without_font_size(self, font_size: float, version: FontVersion):
+        if version in [FontVersion.ALAN_WAKE, FontVersion.ALAN_WAKE_REMASTERED]:
+            self.amount /= font_size
+        else:
+            self.amount *= font_size
+
+        return self
