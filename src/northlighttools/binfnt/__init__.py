acb8f446340df570dea66b969ecf15e46d235e60
Marek Grzyb <marek@grzyb.dev>
2025-07-01 23:33:39 +0200

    Added decompile binary font command

--- src/northlighttools/binfnt/__init__.py
+++ src/northlighttools/binfnt/__init__.py
@@ -1,6 +1,48 @@
+from pathlib import Path
+
 import typer
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+from northlighttools.binfnt.font import BinaryFont
+from northlighttools.rmdp import Annotated
 
 app = typer.Typer(help="Tools for .binfnt files (Binary font files)")
+
+
+@app.command(
+    name="decompile", help="Decompile binary font to editable JSON and bitmap(s)"
+)
+def cmd_decompile(
+    input_file: Annotated[
+        Path,
+        typer.Argument(
+            help="Input .binfnt file path",
+            exists=True,
+            readable=True,
+            file_okay=True,
+            dir_okay=False,
+        ),
+    ],
+    output_dir: Annotated[
+        Path | None,
+        typer.Argument(
+            help="Output directory", writable=True, file_okay=False, dir_okay=True
+        ),
+    ] = None,
+):
+    output_dir = output_dir or input_file.parent / input_file.stem
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    with Progress(
+        SpinnerColumn(finished_text="\u2713"),
+        TextColumn("[progress.description]{task.description}"),
+    ) as progress:
+        task = progress.add_task("Decompiling...", total=1)
+
+        binfnt = BinaryFont(progress, input_file)
+        binfnt.dump(output_dir)
+
+        progress.update(task, advance=1, description="Decompiled successfully")
 
 
 if __name__ == "__main__":
