a28c3ae5ae9fc83facc654ee242d275a3adb4b67
Marek Grzyb <marek@grzyb.dev>
2025-07-01 21:08:05 +0200

    Added chunk size constant

--- src/northlighttools/rmdp/constants.py
+++ src/northlighttools/rmdp/constants.py
@@ -1,2 +1,4 @@
 EPOCH_AS_FILETIME = 116444736000000000  # January 1, 1970 as MS file time
 HUNDREDS_OF_NANOSECONDS = 10000000
+
+CHUNK_SIZE = 1024 * 1024  # 1 MiB, chunk size for reading/writing files
