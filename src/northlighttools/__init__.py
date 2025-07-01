849910d30b5e3b08c1794c269a75f64e5b60d870
Marek Grzyb <marek@grzyb.dev>
2025-07-01 22:09:17 +0200

    Defined typer app for Binary Font tools

--- src/northlighttools/__init__.py
+++ src/northlighttools/__init__.py
@@ -1,8 +1,12 @@
 import typer
 
-from northlighttools import rmdp, string_table
+from northlighttools import binfnt, rmdp, string_table
 
 app = typer.Typer(help="Collection of various tools for Northlight Engine")
+
+app.add_typer(
+    binfnt.app, name="binfnt", help="Tools for .binfnt files (Binary font files)"
+)
 
 app.add_typer(
     rmdp.app, name="rmdp", help="Tools for Remedy Packages (.bin/.rmdp files)"
---
+++ src/northlighttools/binfnt/__init__.py
@@ -0,0 +1,7 @@
+import typer
+
+app = typer.Typer(help="Tools for .binfnt files (Binary font files)")
+
+
+if __name__ == "__main__":
+    app()
