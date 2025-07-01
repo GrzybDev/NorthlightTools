1df689ad983b28578e13dd56a169532267b3299d
Marek Grzyb <marek@grzyb.dev>
2025-07-01 21:53:46 +0200

    Added typer choice enumerators

--- src/northlighttools/rmdp/enumerators/endianness.py
+++ src/northlighttools/rmdp/enumerators/endianness.py
@@ -1,6 +1,11 @@
-from enum import IntEnum
+from enum import Enum, IntEnum
 
 
 class Endianness(IntEnum):
     LITTLE = 0
     BIG = 1
+
+
+class EndiannessChoice(str, Enum):
+    LITTLE = "little"
+    BIG = "big"
--- src/northlighttools/rmdp/enumerators/package_version.py
+++ src/northlighttools/rmdp/enumerators/package_version.py
@@ -1,4 +1,4 @@
-from enum import IntEnum
+from enum import Enum, IntEnum
 
 
 class PackageVersion(IntEnum):
@@ -6,3 +6,10 @@ class PackageVersion(IntEnum):
     ALAN_WAKE_AMERICAN_NIGHTMARE = 7
     QUANTUM_BREAK = 8
     CONTROL = 9
+
+
+class PackageVersionChoice(str, Enum):
+    ALAN_WAKE = "2"
+    ALAN_WAKE_AMERICAN_NIGHTMARE = "7"
+    QUANTUM_BREAK = "8"
+    CONTROL = "9"
